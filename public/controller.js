a899e984a0f9a93be534a152d73800b7371a7977
CharlesHurt <charles@charleshurt.com>
2016-03-10 18:25:43 -0800

    Angular boilerplate 1

---
+++ public/controller.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var flashcardApp = angular.module("flashcardApp", [])
+
+flashcardApp.controller("flashcardController", function($scope) {
+  $scope.hello ='42'
+})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path')
 
+//var Flashcard = require('Flashcard')
+
 var router = express.Router();
 var INDEX_HTML = path.join(__dirname, 'index.html')
 path.resolve(__dirname, 'public', 'index.html')
@@ -9,6 +11,12 @@ console.log(INDEX_HTML);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(INDEX_HTML)
+  //res.render('index', { title: 'Express' });
+});
+
+router.get('/flashcard', function(req, res, next) {
+  //Flashcard.get()
+  //res.sendFile(INDEX_HTML)
   //res.render('index', { title: 'Express' });
 });
 
